eb48241e27c83f2e9323e3ed32f2499e2cebca72
Miller <guf302010@gmail.com>
2021-07-10 17:10:41 +0600

    Constraints setup

--- HW2/ViewController.swift
+++ HW2/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redValue: UILabel!
+    @IBOutlet weak var greenValue: UILabel!
+    @IBOutlet weak var blueValue: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var display: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redValue.lineBreakMode = .byCharWrapping
+        greenValue.lineBreakMode = .byCharWrapping
+        blueValue.lineBreakMode = .byCharWrapping
+        
+        display.layer.cornerRadius = 30
+        display.alpha = 0.2
     }
 
-
+    @IBAction func redSliderAction() {
+        redValue.text = String(redSlider.value)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValue.text = String(greenSlider.value)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueValue.text = String(blueSlider.value)
+    }
 }
 
