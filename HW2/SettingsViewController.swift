ce8b85bbbe6432b685bcf45ba54dc5d4d0d18396
Miller <guf302010@gmail.com>
2021-07-28 22:18:26 +0600

    editing RGB components with textFields

--- HW2/SettingsViewController.swift
+++ HW2/SettingsViewController.swift
@@ -13,6 +13,10 @@ class SettingsViewController: UIViewController {
     
     @IBOutlet weak var display: UIView!
     
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    
     var redPoint: CGFloat = 1.0
     var greenPoint: CGFloat = 1.0
     var bluePoint: CGFloat = 1.0
@@ -32,23 +36,47 @@ class SettingsViewController: UIViewController {
         redSlider.value = Float (redPoint)
         greenSlider.value = Float (greenPoint)
         blueSlider.value = Float (bluePoint)
-    
+        
         display.layer.cornerRadius = 30
         
     }
 
+    @IBAction func redTFAction() {
+        guard let text = redTextField.text else { return }
+        guard let currentValue = Float(text) else {return}
+        redSlider.setValue(currentValue, animated: true)
+        changeColor()
+    }
+    
+    @IBAction func greenTFAction() {
+        guard let text = greenTextField.text else { return }
+        guard let currentValue = Float(text) else {return}
+        greenSlider.setValue(currentValue, animated: true)
+        changeColor()
+    }
+    
+    @IBAction func blueTFAction() {
+        guard let text = blueTextField.text else { return }
+        guard let currentValue = Float(text) else {return}
+        blueSlider.setValue(currentValue, animated: true)
+        changeColor()
+    }
+    
     @IBAction func redSliderAction() {
         redValue.text = String(NSString(format: "%.1f", redSlider.value))
+        redTextField.text = String(NSString(format: "%.2f", redSlider.value))
         changeColor()
     }
     
     @IBAction func greenSliderAction() {
         greenValue.text = String(NSString(format: "%.1f", greenSlider.value))
+        greenTextField.text = String(NSString(format: "%.2f", greenSlider.value))
         changeColor()
     }
     
     @IBAction func blueSliderAction() {
         blueValue.text = String(NSString(format: "%.1f", blueSlider.value))
+        blueTextField.text = String(NSString(format: "%.2f", blueSlider.value))
         changeColor()
     }
     
